1681645797c12292e3bcbe20f03b965d3018798c
Redamancy-11 <tuan.it.fpt@gmail.com>
2026-05-26 16:22:03 +0700

    Add production error logging and nodemon dependency fallback

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION! Shutting down...', err.name, err.message, err.stack);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION! Shutting down...', err.name, err.message, err.stack);
+  process.exit(1);
+});
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
